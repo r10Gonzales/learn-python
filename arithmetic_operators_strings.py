388e26b016416cb42e984f22297be8a09f2957c5
Rio Gonzales <127272104+Nomadcodin9@users.noreply.github.com>
2026-04-24 11:13:48 +0700

    Add: arithmetic operators and strings

---
+++ arithmetic_operators_strings.py
@@ -0,0 +1,40 @@
+# Arithmetic Operators and Strings
+
+# Arithmetic operators:
+# + - * / %
+
+age1 = 12
+age2 = 18
+
+print(age1 + age2)   # addition
+print(age1 * age2)   # multiplication
+print(age1 / age2)   # division
+print(age1 % age2)   # modulus (sisa bagi)
+
+print("-----")
+
+# String basic
+sent1 = "today is a beautiful day"
+print(sent1)
+
+print("-----")
+
+# String concatenation
+first_name = "rio"
+last_name = "gonzales"
+
+print(first_name + last_name)
+print(first_name + " " + last_name)
+
+print("-----")
+
+# String repetition
+print("hi" * 10)
+
+print("-----")
+
+# String indexing & slicing
+sent = "rio was playing basketball"
+
+print(sent[0])     # karakter pertama
+print(sent[0:3])   # dari index 0 sampai sebelum 3
