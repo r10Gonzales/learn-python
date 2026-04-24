d32a0e2385e87eb950274c1cb80e4061a32ea2b7
Rio Gonzales <127272104+Nomadcodin9@users.noreply.github.com>
2026-04-24 11:04:00 +0700

    Initial commit - variables_multiple_assignment

---
+++ variables_multiple_assignment.py
@@ -0,0 +1,34 @@
+# Variable and Multiple Assignment
+
+# Single variable
+age = 24
+print(age)
+
+print("_____")
+
+# String variable
+sentence = "my name is rio"
+print(sentence)
+
+print("_____")
+
+# Multiple Assignment (different values)
+sarah, bob, mike = 16, 22, 19
+print(sarah)
+print(bob)
+print(mike)
+
+print("_____")
+
+#Chained Assignment (same value)
+sarah = bob = mike = 22
+print(sarah)
+print(bob)
+print(mike)
+
+print("_____")
+
+# Mixed Assignment
+name, age = "rio", 24
+print(name)
+print(age)
